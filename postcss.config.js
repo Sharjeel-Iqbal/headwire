2d2ccc19a0bbc94b62a7cc6071659d7d2a674f8f
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2022-06-08 14:38:01 -0700

    added parcel

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.export = {
+  plugins: {
+    'postcss-import': {},
+    'postcss-url': {},
+    'postcss-custom-properties': {},
+    'tailwindcss/nesting': {},
+    tailwindcss: {},
+    autoprefixer: {},
+    cssnano: {},
+  },
+};
