fa00050d4114f3322eeb39f70bb856dd3e9e19f1
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2022-06-09 14:55:44 -0700

    Added fonts and fixed custom styling classes for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ['/blocks/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway: ['Raleway', 'sans-serif'],
+      },
+      padding: {
+        50: '50px',
+        100: '100px',
+      },
+      width: {
+        30: '30%;',
+      },
+      colors: {
+        'headwire-link': '#da7b40',
+      },
+    },
   },
   plugins: [],
 };
