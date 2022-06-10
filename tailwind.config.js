b3c7a3e4fc0719fc1d820327d67cd52cfc1714fd
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2022-06-09 17:59:30 -0700

    Added tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,16 @@ module.exports = {
   content: ['/blocks/**/*.{html,js}'],
   theme: {
     extend: {
+      screens: {
+        phone: { raw: '(max-width: 767.98px)' },
+        tablet: { raw: '(max-width: 1199.98px)' },
+      },
       fontFamily: {
         raleway: ['Raleway', 'sans-serif'],
+      },
+
+      backgroundPosition: {
+        '50%': '50% 50%',
       },
       padding: {
         50: '50px',
@@ -13,6 +21,7 @@ module.exports = {
         30: '30%;',
       },
       colors: {
+        'ez-aem-blue': 'rgb(54, 90, 139)',
         'headwire-link': '#da7b40',
       },
     },
